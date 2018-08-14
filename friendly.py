3897e451d112003b3c2eaacff04da5bd016376be
Scott Idler <scott.a.idler@gmail.com>
2018-08-14 13:06:55 -0700

    added validation regex for friendly timedelta strings

--- friendly.py
+++ friendly.py
@@ -15,20 +15,19 @@ DAYS = '(?P<days>[0-9]+d)?'
 HOURS = '(?P<hours>[0-9]+h)?'
 MINUTES = '(?P<minutes>[0-9]+m)?'
 SECONDS = '(?P<seconds>[0-9]+s)?'
-REGEX = re.compile(WEEKS+DAYS+HOURS+MINUTES+SECONDS)
+PARSE = re.compile(WEEKS+DAYS+HOURS+MINUTES+SECONDS)
+VALIDATE = re.compile('^([0-9]+(w|d|h|m|s))+$')
 
 class FriendlyTimedeltaParseError(Exception):
     def __init__(self, string):
         msg = 'friendly.timedelta parse error for string: ' + string
+        super(FriendlyTimedeltaParseError, self).__init__(msg)
 
 def timedelta(string):
-    try:
-        match = REGEX.match(string)
+    if VALIDATE.match(string):
+        match = PARSE.match(string)
         if not match:
             raise FriendlyTimedeltaParseError(string)
         kwargs = {k:int(v[:-1]) for k,v in match.groupdict().items() if v != None}
         return datetime.timedelta(**kwargs)
-    except:
-        import traceback
-        traceback.print_exc()
-
+    raise FriendlyTimedeltaParseError(string)
