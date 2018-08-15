94156add2b8e46595002cfd54e7292b7d70764d0
Scott Idler <scott.a.idler@gmail.com>
2018-08-15 15:41:57 -0700

    added config.py

---
+++ config.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+config.py
+
+'''
+
+import os
+import traceback
+import configparser
+from ruamel import yaml
+from argparse import Action
+
+from .fmt import *
+from .git import reporoot
+from .isinstance import isiterable
+
+class ConfigLoadError(Exception):
+    def __init__(self, filename, errors):
+        msg = fmt('config load error: filename={filename} errors={errors}')
+        super(ConfigLoadError, self).__init__(msg)
+
+def load_yaml_or_json(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+def load_ini_or_cfg(filename):
+    parser = configparser.ConfigParser()
+    parser.read(filename)
+    sections = {}
+    for section in parser.sections():
+        options = {}
+        for option in parser.options(section):
+            value = parser.get(section, option)
+            options[option] = value
+            sections[section] = options
+    return sections
+
+def load_config(*filenames, must_exist=False):
+    cfg = {}
+    errors = []
+    for filename in [os.path.expanduser(filename) for filename in filenames if filename]:
+        if filename and os.path.isfile(filename):
+            try:
+                cfg.update(load_ini_or_cfg(filename))
+            except Exception as ex1:
+                errors += traceback.format_exc()
+                try:
+                    cfg.update(load_yaml_or_json(filename))
+                except Exception as ex2:
+                    errors += traceback.format_exc()
+                    dbg(errors)
+                    raise ConfigLoadError(filename, errors)
+        elif must_exist:
+            raise FileNotFoundError(filename)
+        else:
+            dbg(filename)
+    return cfg
